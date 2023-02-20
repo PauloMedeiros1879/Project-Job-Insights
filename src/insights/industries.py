842038c05626d395bdcf8aced680f01a1376ab1f
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-02-20 11:25:01 -0400

    feat(industries.py): implementa a função filter_by_industry, que recebe jobs como primeiro parâmetro e industry como segundo, retornando uma lista da coluna industry com todos os empregos - REQ07

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,5 +1,4 @@
 from functools import lru_cache
-from typing import List, Dict
 from src.insights.jobs import read
 
 
@@ -17,19 +16,5 @@ def get_unique_industries(path):
     )
 
 
-def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+def filter_by_industry(jobs, industry):
+    return [filter for filter in jobs if industry == filter["industry"]]
