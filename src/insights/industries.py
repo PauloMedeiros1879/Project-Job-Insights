d79c7a708c3866293b4161cb72a2f16ee0ce9c92
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-02-20 10:31:19 -0400

    feat(industries.py): implementa a função get_unique_industries(), importando read de jobs.py, realiza a leitura e retorna uma lista com valores únicos da coluna industry, utilizando o set() - REQ03

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,20 @@
+from functools import lru_cache
 from typing import List, Dict
+from src.insights.jobs import read
 
 
-def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+@lru_cache
+def get_unique_industries(path):
+    job_read = read(path)
+    return list(
+        set(
+            {
+                industries["industry"]
+                for industries in job_read
+                if industries["industry"] != ""
+            }
+        )
+    )
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
