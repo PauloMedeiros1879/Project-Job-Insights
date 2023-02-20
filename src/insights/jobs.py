473edff756e7701f08528696a0c39a7b1495cbf0
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-02-20 11:12:31 -0400

    feat(jobs.py): implementa a função filter_by_job_type, que recebe jobs como primeiro parâmetro e job_type como segundo, retornando uma lista da coluna job_type com todos os empregos - REQ06

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -15,19 +15,5 @@ def get_unique_job_types(path):
     return list(set({type["job_type"] for type in job_read}))
 
 
-def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+def filter_by_job_type(jobs, job_type):
+    return [type for type in jobs if job_type == type["job_type"]]
