aaa88421bbc55a93dd09061579480dccc5361ba1
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-01 18:25:03 -0400

    test(test_brazilian_jobs): implementa o teste da função read_brazilian_file - REQ11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+path = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+
+    jobsBr = read_brazilian_file(path)
+
+    for dictionary in jobsBr:
+        assert "title" in dictionary
+        assert "salary" in dictionary
+        assert "type" in dictionary
